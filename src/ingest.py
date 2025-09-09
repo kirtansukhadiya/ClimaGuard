095219991fd1513629e59a925bbe8731075b7a56
kirtansukhadiya <kirtanskh@gmail.com>
2025-09-09 18:50:59 -0400

    ingest.py complete with proper sql insertion

--- src/ingest.py
+++ src/ingest.py
@@ -1,18 +1,17 @@
 import requests
-import os
 from dotenv import dotenv_values
 from mysql.connector import (connection)
-from datetime import datetime
+from datetime import datetime, timezone
 
 # Load environment variables
-dotenv_values(".env")
-currentWeatherApiKey = os.getenv("CURRENT_WEATHER_API_URL")
-airPollutionApiKey = os.getenv("AIR_POLLUTION_API_URL")
-historyApiKey = os.getenv("HISTORY_API_URL")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
+env = dotenv_values(".env")
+currentWeatherApiKey = env.get("CURRENT_WEATHER_API_URL")
+airPollutionApiKey = env.get("AIR_POLLUTION_API_URL")
+historyApiKey = env.get("HISTORY_API_URL")
+db_user = env.get("DB_USER")
+db_password = env.get("DB_PASSWORD")
+db_host = env.get("DB_HOST")
+db_name = env.get("DB_NAME")
 
 rw = requests.get(currentWeatherApiKey) # response for current weather from open weather api
 ra = requests.get(airPollutionApiKey) # response for air quality from open weather api
@@ -21,11 +20,14 @@ rh = requests.get(historyApiKey) # response for historical weather from open wea
 rw_data = rw.json()
 ra_data = ra.json()
 rh_data = rh.json()
+#print(rh_data)
 
 # Functions to extract relevant data
 def get_current_weather():
     weather = {
-        "temperature": rw_data["main"]["temp"],
+        "temp_c": rw_data["main"]["temp"],
+        "min_temp_c": rw_data["main"]["temp_min"],
+        "max_temp_c": rw_data["main"]["temp_max"],
         "humidity": rw_data["main"]["humidity"],
         "pressure": rw_data["main"]["pressure"],
         "weather_description": rw_data["weather"][0]["description"],
@@ -44,24 +46,30 @@ def get_air_quality():
         "no2": ra_data["list"][0]["components"]["no2"],
         "o3": ra_data["list"][0]["components"]["o3"],
         "so2": ra_data["list"][0]["components"]["so2"],
-        "pm2_5": ra_data["list"][0]["components"]["pm2_5"],
+        "pm25": ra_data["list"][0]["components"]["pm2_5"],
         "pm10": ra_data["list"][0]["components"]["pm10"],
         "nh3": ra_data["list"][0]["components"]["nh3"]
     }
     return air_quality
 
 def get_historical_weather():
-    historical_weather = {
-        "temperature": rh_data["list"][0]["main"]["temp"],
-        "humidity": rh_data["list"][0]["main"]["humidity"],
-        "pressure": rh_data["list"][0]["main"]["pressure"],
-        "weather_description": rh_data["list"][0]["weather"][0]["description"],
-        "wind_speed": rh_data["list"][0]["wind"]["speed"],
-        "wind_direction": rh_data["list"][0]["wind"]["deg"],
-        "cloudiness": rh_data["list"][0]["clouds"]["all"],
-         
-    }
-    return historical_weather
+    weather_dicts = []
+    for entry in rh_data["list"]:
+        row = {
+            "temp_c": entry["main"]["temp"],
+            "humidity": entry["main"]["humidity"],
+            "pressure": entry["main"]["pressure"],
+            "weather_description": entry["weather"][0]["description"],
+            "wind_speed": entry["wind"]["speed"],
+            "wind_direction": entry["wind"]["deg"],
+            "cloudiness": entry["clouds"]["all"],
+            "ts": datetime.fromtimestamp(entry["dt"], tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        weather_dicts.append(row)
+    return weather_dicts
+
+all_data = {**get_current_weather(), **get_air_quality()}
+historical_data = get_historical_weather()
 
 #SQL connection and insertion
 try :
@@ -73,11 +81,22 @@ try :
 }
 
     db = connection.MySQLConnection(**config)
+    print("Connected to %s database"% db.database)
     cursor = db.cursor()
-    cursor.execute = ("INSERT INTO weather_raw "
-            "(city, ts, temp_c, min_temp_c, max_temp_c, wind_speed, humidity, aqi, pm25, pm10, weather_description, wind_direction, cloudiness, co, no_, no2, o3, so2, nh3)"
-            "VALUES (:city, :ts, :temp_c, :min_temp_c, :max_temp_c, :wind_speed, :humidity, :aqi, :pm25, :pm10, :weather_description, :wind_direction, :cludiness, :co, :no_, :no2, :o3, :so2, :nh3)", get_air_quality(), get_current_weather(), get_historical_weather())
+    columns = ', '.join(all_data.keys())                 
+    placeholders = ', '.join(['%s'] * len(all_data))         
+    values = tuple(all_data.values())
+    query = f"INSERT INTO weather_raw ({columns}) VALUES ({placeholders})"
+    cursor.execute(query, values)
+
+    hist_columns =', '.join(historical_data[0].keys())
+    hist_placeholders = ', '.join(['%s'] * len(historical_data[0]))
+    hist_values = [tuple(d.values()) for d in historical_data]
+    hist_query = f"INSERT INTO weather_raw ({hist_columns}) VALUES ({hist_placeholders})"
+    cursor.executemany(hist_query, hist_values)
     db.commit()
     print("Data inserted successfully")
+    cursor.close()
+    db.close()
 except Exception as e:
     print(f"Error: {e}")
