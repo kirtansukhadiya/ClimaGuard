85b055591df0a0f19f5dfce9c2b5c00fe3170355
kirtansukhadiya <kirtanskh@gmail.com>
2025-09-11 14:25:24 -0400

    made engine to connect mysql

--- src/features.py
+++ src/features.py
@@ -1,3 +1,15 @@
-from ingest import get_current_weather, get_air_quality
 import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy import create_engine, URL
+from dotenv import dotenv_values
 
+env = dotenv_values(".env")
+db_user =env.get("DB_USER"),
+db_password =env.get("DB_PASSWORD"),
+db_host =env.get("DB_HOST"),
+db_name =env.get("DB_NAME")
+
+engine = create_engine(f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}")
+
+df = pd.read_sql_table('weather_row', con=engine)
+#df = pd.read_sql_query('SELECT * FROM your_table_name WHERE column = "value"', con=engine)
